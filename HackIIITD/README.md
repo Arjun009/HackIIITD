ca5908ea20d0777dc44f72f64cf478cea44781b5
Arjun Mohnot <arjunmohnot@gmail.com>
2019-08-24 11:09:41 +0530

    'changes'

--- HackIIITD/README.md
+++
@@ -1,2 +0,0 @@
-# HackIIITD
-Link going live soon!!
--- models/users.js
+++
@@ -1,8 +0,0 @@
-let mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var users = new Schema({
-    name:  String,
-    time: String,
-    
-  });
-module.exports = mongoose.model('Users', users)
